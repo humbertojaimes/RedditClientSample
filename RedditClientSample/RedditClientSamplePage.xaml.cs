1b993ecd683afe081b290eb09f92222eeea346f8
Humberto Jaimes <humberto.jaimes@outlook.com>
2018-02-19 22:22:25 -0600

    Initial check-in of module RedditClientSample

---
+++ RedditClientSample/RedditClientSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RedditClientSample
+{
+    public partial class RedditClientSamplePage : ContentPage
+    {
+        public RedditClientSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}

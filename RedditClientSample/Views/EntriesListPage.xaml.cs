6a668f649da230a54a06f24fccd0912ab38fe63c
Humberto Jaimes <humberto.jaimes@outlook.com>
2018-02-21 05:41:12 -0600

    Dismiss all button implemented

--- RedditClientSample/Views/EntriesListPage.xaml.cs
+++ RedditClientSample/Views/EntriesListPage.xaml.cs
@@ -32,14 +32,29 @@ namespace RedditClientSample.Views
 
         async void Handle_Tapped(object sender, TappedEventArgs e)
         {
+            
             var context = BindingContext as EntriesListViewModel;
             Image image = sender as Image;
-
             var cell = image.Parent.Parent as ViewCell;
             cell.View.FadeTo(0, 1000, Easing.Linear);
             await Task.Delay(1100);
-            context.TopEntries.Remove(cell.BindingContext as RedditEntry);
+            var selectedEntry = cell.BindingContext as RedditEntry;
+            context.TopEntries.Remove(selectedEntry);
         }
+
+        async void DismissAll_Click_Handle(object sender, EventArgs e)
+        {
+
+            var context = BindingContext as EntriesListViewModel;
+            Button button = sender as Button;
+            var mainStack = button.Parent as View; 
+            mainStack.FadeTo(0, 1500, Easing.Linear);
+            await Task.Delay(1600);
+            var selectedEntry = mainStack.BindingContext as RedditEntry;
+            context.TopEntries.Clear();
+            mainStack.FadeTo(1, 1500, Easing.Linear);
+        }
+
 
     }
 }

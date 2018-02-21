080e2b6a1671e74d0f1e15f01a7e095903a43ff4
Humberto Jaimes <humberto.jaimes@outlook.com>
2018-02-21 05:01:50 -0600

    Pull to refresh added

--- RedditClientSample/ViewModels/EntriesListViewModel.cs
+++ RedditClientSample/ViewModels/EntriesListViewModel.cs
@@ -5,6 +5,8 @@ using RedditClientSample.Interfaces;
 using RedditClientSample.Models;
 using RedditClientSample.Services.LocalServices;
 using RedditClientSample.Services.RestServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace RedditClientSample.ViewModels
 {
@@ -18,6 +20,40 @@ namespace RedditClientSample.ViewModels
             set { topEntries = value; OnPropertyChanged(); }
         }
 
+        private bool isRefreshing;
+
+        public bool IsRefreshing
+        {
+            get { return isRefreshing; }
+            set
+            {
+                SetProperty(ref isRefreshing, value);
+            }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    if (!IsBusy)
+                    {
+                        IsRefreshing = true;
+                        IsBusy = true;
+                        if (useMockData)
+                            redditService = new LocalRedditService();
+                        else
+                            redditService = new RestRedditService();
+
+                        Title = "Top Reddit Entries";
+                        TopEntries = new ObservableCollection<RedditEntry>(await redditService.GetTopEntries());
+                        IsRefreshing = false;
+                        IsBusy = false;
+                    }
+                });
+            }
+        }
 
         bool useMockData = false;
         IRedditService redditService;
@@ -36,7 +72,10 @@ namespace RedditClientSample.ViewModels
 
             Title = "Top Reddit Entries";
             TopEntries = new ObservableCollection<RedditEntry>(await redditService.GetTopEntries());
-     
+
         }
+
+
+
     }
 }
